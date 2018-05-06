355f1383faaefe6b2c5dd96257993750de920291
ysjgithub <2534123987@qq.com>
2018-05-06 23:07:37 +0800

    红包ui，赞助商ui

--- pages/index/index.js
+++ pages/index/index.js
@@ -4,7 +4,10 @@ const Zan = require('../../dist/index');
 var Data = require('../../data/index.js');
 //全局变量
 var all = require('../../data/all.js');
-var router = require('../../router.js')
+//功能模块，充值钱
+var Function = require('../../systemcall/function.js');
+var router = require('../../router.js');
+
 var that = null;
 var app = getApp();
 
@@ -17,16 +20,17 @@ Page({
     polyline: Data.polyline,    //路线
     controls: Data.controls,    //地图上的控点
     imformation: "您需要充值押金或余额才能借伞！", //提示信息
-    needmoney: wx.getStorageSync("needmoney"),
-    usedtime:{hour:"00",minute:"15",second:"20"},
-    starttime: { hour: "09", minute: "12", second: "20"}
+    needmoney: wx.getStorageSync("needmoney"),  //需要充的钱，控制顶部条
+    usedtime: { hour: "00", minute: "15", second: "20" }, //使用中已经使用的时间
+    starttime: { hour: "09", minute: "12", second: "20" }  //使用开始的时间
   },
+  //初始化
   onLoad: function () {
     that = this;
-    this.changeicon();    //查看当前状态
-    console.log(wx.getStorageSync("needmoney"));.0
-  },
-  onShow: function () {
+    this.changeicon();    //查看当前状态改变底部的按钮图片
+    if (wx.getStorageSync("redbag")!==0) {
+      this.showDialog();
+    }
   },
   onReady: function () {
     // 使用 wx.createMapContext 获取 map 上下文
@@ -34,7 +38,6 @@ Page({
     this.getCenterLocation();
     this.moveToLocation();
   },
-
   // 得到地图中心的位置
   getCenterLocation: function () {
     this.mapCtx.getCenterLocation({
@@ -48,78 +51,24 @@ Page({
   moveToLocation: function () {
     this.mapCtx.moveToLocation()
   },
-
-  //移动标记点
+  //移动标记点，后继将用户位置替换为伞的移动
   translateMarker: function () {
     this.mapCtx.translateMarker({
     })
   },
-  //点击去充值
+
+  //点击去充值，充值到能借伞的程度
   addmoney: function () {
-    // 充值记录
-    wx.request({
-      url: router.user.paymentUrl,
-      data: {
-        rechargeNum: 30, //充值金额
-        rechargeType: 1 //充值类型
-      },
-      method: 'POST',
-      header: {
-        'authenticate': wx.getStorageSync('client_sign'), //唯一标识
-        'content-type': 'application/x-www-form-urlencoded'
-      },
-      success: function (res) {
-
-        /***开始支付请求***/
-        wx.requestPayment({
-
-          //时间戳从1970年1月1日00:00:00至今的秒数,即当前的时间
-          timeStamp: res.data.timeStamp,
-          //随机字符串，长度为32个字符以下。
-          nonceStr: res.data.nonceStr,
-          //统一下单接口返回的 prepay_id 参数值，提交格式如：prepay_id=*
-          package: res.data.package,
-          //签名算法，暂支持 MD5
-          signType: 'MD5',
-          //签名
-          paySign: res.data.paySign,
-          //支付成功，开始更新用户余额
-          success: function (res) {
-            wx.setStorageSync("needmoney", 0);
-            that.setData({
-              needmoney: 0
-            })
-            // 更新余额
-            wx.request({
-              url: res.user.charge_over,
-              data: {
-                "over": 30, //充值金额
-              },
-              method: 'POST',
-              header: {
-                'authenticate': wx.getStorageSync('client_sign'), //唯一标识
-                'content-type': 'application/x-www-form-urlencoded'
-              },
-              success: function (res) {
-              }//endsuccess
-            })//endreqquest
-          },
-          fail: function (res) {
-          },
-          complete: function (res) {
-            wx.setStorageSync("needmoney", 0);
-            that.setData({
-              needmoney: 0
-            })
-          }
-        })
-      }
+    Function.addmoney(wx.getStorageSync("needmoney"));
+    that.setData({
+      needmoney: 0
     })
   },
 
   //点击地图上的controls
   click: function (num) {
     switch (num) {
+      //点击定位按钮
       case Data.GET_POSITION:
         this.getCenterLocation();
         this.moveToLocation();
@@ -135,34 +84,40 @@ Page({
   controltap(e) {
     console.log(e.controlId);
     this.click(e.controlId);
-    // 扫码
+    // 如果不是借伞还伞，跳转
     if (e.controlId >= 1)
       wx.navigateTo({
         url: Data.pages[e.controlId],
       })
-    // 跳转
+    //是借伞还伞
     else if (e.controlId === 4 && app.globalData.CurrentStatus.status !== 0) {
       that.getscan();
-    } 
-
+    }
   },
 
   // 显示弹窗
   showDialog: function () {
     Zan.Dialog(Data.Dialogs[0]).then(({ type }) => {
       // type 可以用于判断具体是哪一个按钮被点击
-
       switch (type) {
+        //点击返回,用于测试,改变当前的状态为未借伞
         case 'back':
           app.globalData.CurrentStatus = all.Statuses.Unusing;
           that.changeicon();
           break;
+        //点击确定获得红包
+        case 'get':
+          // 得到红包
+          wx.navigateTo({
+            url: '../myredbag/myredbag?redbag=' + wx.getStorageSync("redbag"),
+          })
+          break;
         default:
           break;
       }
-      console.log('=== dialog with custom buttons ===', `type: ${type}`);
     });
   },
+  //根据当前的状态改变底部的图像
   changeicon: function () {
     let newcontrols = that.data.controls;
     newcontrols[4].iconPath = app.globalData.CurrentStatus.src;
@@ -171,6 +126,8 @@ Page({
       controls: newcontrols
     })
   },
+
+  //扫码
   getscan: function () {
     wx.scanCode({
       onlyFromCamera: true,
@@ -178,14 +135,14 @@ Page({
       seccess: function (res) {
       }, fail(res) {
       }, complete(res) {
-        if (app.globalData.CurrentStatus===all.Statuses.Using){
+        //根据当前的状态选择后台传输的url
+        if (app.globalData.CurrentStatus === all.Statuses.Using) {
           app.globalData.CurrentStatus = all.Statuses.Unusing;
           that.changeicon();
-        }else{
+        } else {
           app.globalData.CurrentStatus = all.Statuses.Using;
           that.changeicon();
         }
-        
       }
     })
   }
